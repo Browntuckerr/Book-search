151896bd910a3dca35389d6b165a76b6bb765d3d
Browntuckerr <82062612+Browntuckerr@users.noreply.github.com>
2021-09-19 20:43:01 -0400

    Add files via upload

---
+++ src/utils/mutations.js
@@ -0,0 +1,66 @@
+import gql from "graphql-tag";
+
+export const LOGIN_USER = "gql"`
+mutation loginUser($email: String!, $password: String!){
+    login(email: $email, password: $password) {
+        token
+        user{
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation addUser($username: String!, $password: String!, $email: String!) {
+    addUser(username: $username, password: $password, email: $email) {
+        token
+        user {
+            _id 
+            username
+            }
+        }
+    }
+}
+`;
+
+export const SAVE_BOOK = gql `
+mutation saveBook($book: savedBook!){
+    saveBook(book: $book)
+    {
+        username
+        email
+        bookCount
+        savedBooks{
+            bookId
+            authors
+            image
+            link
+            title
+            description
+        }
+    }
+}
+`;
+
+export const REMOVE_BOOK =gql `
+mutation removeBook($bookId: String!){
+    removeBook(bookId: $bookId) {
+        _id
+        username
+        email
+        bookCount
+        savedBooks{
+            _id
+            bookId
+            authors
+            image
+            link
+            title
+            description 
+        }
+    }
+}
+`;
+
